ba718a180d8b9e67abf5719fae11b59d2f30dbef
csjkevin <csjkevin@outlook.com>
2022-05-10 18:07:37 +0800

    feat: redirect the root path to login page

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    { path: '/', redirect: '/login' },
     {
       path: '/login',
       component: '@/pages/Login',
